938453dab1137d66d3f9c20dd7943917b5891e96
Ravi <yadavravi7770@gmail.com>
2021-07-24 22:36:21 +0530

    All About this Project
    Signed-off-by: Ravi <yadavravi7770@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './pages';
+import About from './pages/about';
+import Menu from './pages/menu';
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-2xl text-purple-900">Hello World</h1>
-    </div>
+    <>
+      <Navbar/>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/about" exact component={About}/>
+        <Route path="/menu" exact component={Menu}/>
+      </Switch>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div className="flex flex-col justify-center items-center bg-white h-screen font-mono py-40">
+           <img src="\images\EGG.jpg" alt="" />
+            <div className="flex flex-col justify-center items-center">
+                <h2 className="text-2xl mb-2">Egg Muffins</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Content;
